bbc760b8c787b8fe8f83c8490c75b14f6885a7f1
JFFica <jorg.fuentesf@duocuc.cl>
2026-05-14 23:54:01 -0400

    backend inicial spring boot

---
+++ src/main/java/cl/devops/innovatech_backend/InnovatechBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.devops.innovatech_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovatechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovatechBackendApplication.class, args);
+	}
+
+}
